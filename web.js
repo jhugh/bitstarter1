bf651c4281e3c6e320bfc9000ca67c3c7dea042f
Hugh <hugh@Startuptu-VBox.(none)>
2013-07-05 13:05:39 +0100

    Updated web.js yet again

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 var buf = new Buffer(255);
 var fn  = fs.readFileSync('index.html');
-var len = buf.write(fn,0);
+//var len = buf.write(fn,0);
 
 app.get('/', function(request, response) {
   response.send('Hello world 2!');
