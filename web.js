22e3274479d63d2ca199efd614156faf4a91aacc
Hugh <hugh@Startuptu-VBox.(none)>
2013-07-05 12:48:05 +0100

    Updated web.js yet again

--- web.js
+++ web.js
@@ -2,14 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(255);
-var fn  = fs.readFileSync('index.html');
-
-buf.write(fn);
+//var buf = new Buffer(255);
+//var fn  = fs.readFileSync('index.html');
+//buf.write(fn);
 
 app.get('/', function(request, response) {
-  //response.send('Hello world 2!');
-  response.send(buf.toString()); 
+  response.send('Hello world 2!');
+  //response.send(buf.toString()); 
 });
 
 var port = process.env.PORT || 5000;
