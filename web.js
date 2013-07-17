f769e7335f41354366050cdd2772c650a6a95a8c
Hugh <hugh@Startuptu-VBox.(none)>
2013-07-17 23:02:10 +0100

    use Bootstrap html/js/css for HW4

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(3000);
+var buf = new Buffer(15000);
 var fn  = fs.readFileSync('index.html','utf8');
 var len = buf.write(fn,0,'utf8');
 
@@ -16,7 +16,8 @@ app.get('/', function(request, response) {
 
 });
 
-var port = process.env.PORT || 5000;
+//var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
